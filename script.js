c51633e627914dfcc0e53a1ef344f51c2a2c7441
Mukesh Jain <99585972+jaingeek@users.noreply.github.com>
2026-06-29 13:14:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const SHEET_URL = "https://opensheet.elk.sh/1GiZA69JfSs-RDS4fsRqpp4kBBvraNJbxTqvbhOVl2Is/Table%201";
-
-async function loadData() {
-    const response = await fetch(SHEET_URL);
-    const data = await response.json();
-
-    console.log(data);
-
-    document.getElementById("results").innerHTML =
-        "<h2>Total Saints: " + data.length + "</h2>";
-}
-
-loadData();
+alert("Script Loaded Successfully");
